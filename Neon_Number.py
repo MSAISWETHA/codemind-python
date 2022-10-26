d2a6500aabcdc3304d35f4816e19946b60f445e0
MSAISWETHA <84618604+MSAISWETHA@users.noreply.github.com>
2022-10-26 21:31:19 +0530

    n=int(input()) x=n*n z=0 while x>0:     l=x%10     z+=l     x=x//10 if n==z:     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,13 +1,11 @@
-N=int(input())
-square=N**2
-sum=0
-while(square!=0):
-    rem=square%10
-    sum=sum+rem
-    square=square//10
-if (sum==N):
+n=int(input())
+x=n*n
+z=0
+while x>0:
+    l=x%10
+    z+=l
+    x=x//10
+if n==z:
     print("Neon Number")
 else:
     print("Not Neon Number")
-    
-    
